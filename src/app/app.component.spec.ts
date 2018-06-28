6dec7357ccb1621a78196dffb1adecced9f2a2be
Dinh Duong <0961020@gmail.com>
2018-06-28 10:39:50 +0800

    Add CI test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [FormsModule, ChartsModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -22,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('DICS online checking');
   }));
 });
